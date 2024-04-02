6d8abbd4f6716461dc6df6cbbcd6b0f3a36b0c24
Elliot-KJW <eidechse4@hanmail.net>
2024-04-03 01:58:45 +0900

    Clear:task 2 solved

--- task2/port_scanner.py
+++ task2/port_scanner.py
@@ -1,22 +1,25 @@
 import socket
 import sys
 
+
 def port_scanner(target_ip, start_portno, end_portno):
-
     for port_number in range(start_portno, end_portno):
-        #TODO: your code here
-
+        # TODO: your code here
         try:
-            #TODO: your code here
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target_ip, port_number))
+            s.send('hello'.encode())
+            print(port_number, "is opened")
+            s.close()
         except ConnectionRefusedError:
             continue
         except TimeoutError:
             continue
 
-if __name__ == '__main__':
-        
-       target_ip = sys.argv[1]
-       start_portno = int(sys.argv[2])
-       end_portno = int(sys.argv[3])
 
-       port_scanner(target_ip, start_portno, end_portno)
+if __name__ == '__main__':
+    target_ip = sys.argv[1]
+    start_portno = int(sys.argv[2])
+    end_portno = int(sys.argv[3])
+
+    port_scanner(target_ip, start_portno, end_portno)
