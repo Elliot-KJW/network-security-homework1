633496ece479123d6430de349ba7b56b44aa1bff
Elliot-KJW <eidechse4@hanmail.net>
2024-04-03 00:03:56 +0900

    Clear:task 1 solved

--- task1/echo_client.py
+++ task1/echo_client.py
@@ -6,11 +6,13 @@ BUF_SIZE = 4096
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #TODO: connect to the server
+socket.connect((IP, PORT))
 
 try:
     while True:
         your_input = input().encode()
-        #TODO: send your input string to the server 
+        #TODO: send your input string to the server
+        socket.sendall(your_input)
         response = socket.recv(BUF_SIZE)
         if not response: break
         print(response.decode())
